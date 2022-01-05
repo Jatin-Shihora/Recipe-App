d924e468ed5dc0cc8d9014d6ac1cad333ab85554
Jatin Shihora <jatinshihora0123@gmail.com>
2022-01-05 19:27:47 +0530

    Added Row layout

--- app/src/main/java/com/example/reciepe_app_kt/MainActivity.kt
+++ app/src/main/java/com/example/reciepe_app_kt/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -36,17 +38,30 @@ class MainActivity : ComponentActivity() {
                 )
                 Column(modifier = Modifier.padding(16.dp),
                 ) {
-                    Text(text = "Happy meal" ,
-                    style = TextStyle(fontSize = 26.sp)
+                    Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "Happy meal" ,
+                            style = TextStyle(fontSize = 26.sp)
+                        )
+                    Text(text="$5.99",
+                        style = TextStyle(
+                            color = Color.Green,
+                            fontSize = 17.sp),
+                        modifier = Modifier.align(Alignment.CenterVertically)
                     )
+                }
+
                     Spacer(modifier = Modifier.padding(top=10.dp))
                     Text(text="800 Calories",
                         style = TextStyle(fontSize = 17.sp))
                     Spacer(modifier = Modifier.padding(top=10.dp))
-                    Text(text="$5.99",
-                        style = TextStyle(
-                            color = Color.Green,
-                            fontSize = 17.sp))
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                            Text(text = "Order Now")
+                    }
                 }
             }
 
