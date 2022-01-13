a23e1c0222f0fce556c1092ecf86a630746253de
Jatin Shihora <jatinshihora0123@gmail.com>
2022-01-13 11:18:50 +0530

    Used constraintlayout in compose

--- app/src/main/java/com/example/reciepe_app_kt/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/example/reciepe_app_kt/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,26 +1,47 @@
 package com.example.reciepe_app_kt.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CircularIndeterminateProgressBar(
     isDisplayed:Boolean,
 ){
-    if(isDisplayed){
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(50.dp), horizontalArrangement = Arrangement.Center)
-        {
-            CircularProgressIndicator(color =MaterialTheme.colors.primary)
-        }
+    if (isDisplayed) ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (progressBar, text) = createRefs()
+        val topGuideline = createGuidelineFromTop(0.2f)
+
+        CircularProgressIndicator(
+            modifier = Modifier.constrainAs(progressBar) {
+                top.linkTo(topGuideline)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            color = MaterialTheme.colors.primary
+        )
+        Text(
+            text = "Loading recipes...",
+            style = TextStyle(
+                color = MaterialTheme.colors.primary,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(progressBar.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        )
     }
 
 }
